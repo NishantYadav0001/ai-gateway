0be2eaa4dedeab0f6e060d5f8a75c265d93b1db8
NishantYadav0001 <Nishantyaduvanshi056@gmail.com>
2026-05-29 12:27:56 +0530

    fix: routing the app to ui

--- src/main/java/com/smartcache/gateway/controller/HealthController.java
+++ src/main/java/com/smartcache/gateway/controller/HealthController.java
@@ -1,12 +1,22 @@
 package com.smartcache.gateway.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.view.RedirectView;
 
-@RestController
+@Controller // Note: Changed from @RestController to @Controller to allow redirects
 public class HealthController {
 
+    // 1. When you visit the main URL, instantly redirect to Swagger
     @GetMapping("/")
+    public RedirectView redirectToSwagger() {
+        return new RedirectView("/swagger-ui/index.html");
+    }
+
+    // 2. Give Render a dedicated, silent endpoint just for health checks
+    @GetMapping("/health")
+    @ResponseBody
     public String healthCheck() {
         return "Gateway is Live and Healthy!";
     }
